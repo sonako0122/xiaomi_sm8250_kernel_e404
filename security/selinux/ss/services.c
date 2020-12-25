7f85935b4320c36d05c339b4cf630449304fcadb
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 12:41:56 -0800

    selinux: Remove audit dependency
    Squash of:
     selinux: Remove audit dependency (commit1)
     selinux: don't require auditing (commit2)
     selinux: Allow audit to be disabled (commit3)
    
    commit 1:
      Auditing comes with a lot of overhead due to string assembly via
      vsnprintf. It isn't actually needed to make SELinux work, so remove
      SELinux's artificial dependency on it to make it possible to use SELinux
      without the unneeded overhead.
    
    commit 2:
      Audit is bad. It affects performance under all circumstances. It's also
      dirty.
    
      From LWN: "Andy submitted a patch to fix this particular problem, but he
      didn't stop there. He has come to the conclusion that the audit
      subsystem is beyond repair, so his patch marks the whole thing as being
      broken, making it generally inaccessible. He cited a number of problems
      beyond this security issue: it hurts performance even when it is not
      being used, it is not (in his mind) reliable, it has problems with
      various architectures, and "its approach to freeing memory is
      terrifying." All told, Andy said, we're better off without it"
    
    commit 3:
      SELinux doesn't actually need audit support. In fact, auditing is not
      particularly useful on production kernels, where minimal debugging is
      needed.
    
    Co-authored-by: kdrag0n <dragon@khronodragon.com>
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3575,6 +3575,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3597,6 +3598,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
