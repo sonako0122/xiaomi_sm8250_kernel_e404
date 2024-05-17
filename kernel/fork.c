820f85868848eabca987fb08ab30ec57791315a1
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-05-17 17:13:24 +0700

    Merge tag 'ASB-2024-05-05_4.19-stable' of https://android.googlesource.com/kernel/common
    https://source.android.com/docs/security/bulletin/2024-05-01
    CVE-2023-4622
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2093,6 +2093,7 @@ static __latent_entropy struct task_struct *copy_process(
 					      O_RDWR | O_CLOEXEC);
 		if (IS_ERR(pidfile)) {
 			put_unused_fd(pidfd);
+			retval = PTR_ERR(pidfile);
 			goto bad_fork_free_pid;
 		}
 		get_pid(pid);	/* held by pidfile now */
@@ -2208,10 +2209,6 @@ static __latent_entropy struct task_struct *copy_process(
 		goto bad_fork_cancel_cgroup;
 	}
 
-	/* past the last point of failure */
-	if (pidfile)
-		fd_install(pidfd, pidfile);
-
 	init_task_pid_links(p);
 	if (likely(p->pid)) {
 		ptrace_init_task(p, (clone_flags & CLONE_PTRACE) || trace);
@@ -2259,6 +2256,9 @@ static __latent_entropy struct task_struct *copy_process(
 	spin_unlock(&current->sighand->siglock);
 	syscall_tracepoint_update(p);
 	write_unlock_irq(&tasklist_lock);
+
+	if (pidfile)
+		fd_install(pidfd, pidfile);
 
 	proc_fork_connector(p);
 	sched_post_fork(p);
