cac3726f06b1835da399a13288c84cee5771d7b7
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-06-04 13:24:57 +0700

    techpack/gpu: atomic: reset back changes to certain point
    techpack/xxx/xxx: 44db376643ab726faeda4da002ab168a1ed5b259
    drivers/xxx/xxx: 0e5107f00a0e4f97b2ccd50a8de220d33405421a
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -31,10 +31,8 @@
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
 #include <drm/drm_writeback.h>
-#include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
-#include <linux/pm_qos.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -1299,21 +1297,9 @@ drm_atomic_get_connector_state(struct drm_atomic_state *state,
 		struct __drm_connnectors_state *c;
 		int alloc = max(index + 1, config->num_connector);
 
-		if (state->connectors_preallocated) {
-			state->connectors_preallocated = false;
-			c = kmalloc(alloc * sizeof(*state->connectors),
-				    GFP_KERNEL);
-			if (!c)
-				return ERR_PTR(-ENOMEM);
-			memcpy(c, state->connectors,
-			       sizeof(*state->connectors) * state->num_connector);
-		} else {
-			c = krealloc(state->connectors,
-				     alloc * sizeof(*state->connectors),
-				     GFP_KERNEL);
-			if (!c)
-				return ERR_PTR(-ENOMEM);
-		}
+		c = krealloc(state->connectors, alloc * sizeof(*state->connectors), GFP_KERNEL);
+		if (!c)
+			return ERR_PTR(-ENOMEM);
 
 		state->connectors = c;
 		memset(&state->connectors[state->num_connector], 0,
@@ -2580,8 +2566,8 @@ static void complete_signaling(struct drm_device *dev,
 	kfree(fence_state);
 }
 
-static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
-				   struct drm_file *file_priv)
+int drm_mode_atomic_ioctl(struct drm_device *dev,
+			  void *data, struct drm_file *file_priv)
 {
 	struct drm_mode_atomic *arg = data;
 	uint32_t __user *objs_ptr = (uint32_t __user *)(unsigned long)(arg->objs_ptr);
@@ -2739,31 +2725,6 @@ out:
 
 	drm_modeset_drop_locks(&ctx);
 	drm_modeset_acquire_fini(&ctx);
-
-	return ret;
-}
-
-int drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
-			  struct drm_file *file_priv)
-{
-	/*
-	 * Optimistically assume the current task won't migrate to another CPU
-	 * and restrict the current CPU to shallow idle states so that it won't
-	 * take too long to finish running the ioctl whenever the ioctl runs a
-	 * command that sleeps, such as for an "atomic" commit. Apply this
-	 * restriction to the prime CPU as well in anticipation of it processing
-	 * the DRM IRQ and any other display commit work, so that it wakes up
-	 * now if it's in a deep idle state.
-	 */
-	struct pm_qos_request req = {
-		.type = PM_QOS_REQ_AFFINE_CORES,
-		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
-	};
-	int ret;
-
-	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
-	ret = __drm_mode_atomic_ioctl(dev, data, file_priv);
-	pm_qos_remove_request(&req);
 
 	return ret;
 }
