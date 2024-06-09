460f3468c7024c1f13e7a167f6f5d05b77ddc76f
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-06-09 21:57:16 +0700

    gpu: drm/atomic: only kick devfreq boost when committing frame
    cpu input boost is kinda useless after Emanuel do some changes to sbalance,
    because it is now alr affined to big cores and thus boosting is not needed anymore.
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,7 +34,6 @@
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/pm_qos.h>
 
 #include "drm_crtc_internal.h"
@@ -2621,7 +2620,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		#ifdef CONFIG_KPROFILES
 		if (kp_active_mode() == 0 || kp_active_mode() == 2 || kp_active_mode() == 3) {
-			cpu_input_boost_kick();
 			devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 		}
 		#endif
