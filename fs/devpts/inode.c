1b7d971ec2f36bc14d2984c7d140226aa57463ff
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-06-10 14:13:23 +0000

    KernelSU: devpts: fix 'pm' command under devpts inode
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -615,6 +615,9 @@ extern int ksu_handle_devpts(struct inode*);
 #endif
 void *devpts_get_priv(struct dentry *dentry)
 {
+	#ifdef CONFIG_KSU
+	ksu_handle_devpts(dentry->d_inode);
+	#endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
