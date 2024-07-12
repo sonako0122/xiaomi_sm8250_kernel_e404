19818a895e129dc8312b9a770ea2ece2878b613d
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-07-12 22:40:37 +0700

    Revert "sched/fair: Optimize test_idle_cores() for !SMT"
    This reverts commit 8ec2d59ff47ec377c632d04f8dd96f0524ccf49b.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6899,11 +6899,9 @@ static inline bool test_idle_cores(int cpu, bool def)
 {
 	struct sched_domain_shared *sds;
 
-	if (static_branch_likely(&sched_smt_present)) {
-		sds = rcu_dereference(per_cpu(sd_llc_shared, cpu));
-		if (sds)
-			return READ_ONCE(sds->has_idle_cores);
-	}
+	sds = rcu_dereference(per_cpu(sd_llc_shared, cpu));
+	if (sds)
+		return READ_ONCE(sds->has_idle_cores);
 
 	return def;
 }
