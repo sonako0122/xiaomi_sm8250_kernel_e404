50d054c862054b935484c3123ab8084ece588afb
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-09-02 21:47:50 +0700

    Revert "devfreq_boost: increase frequency for the boost"
    This reverts commit f0a32a061e9971aeaac572b34271dd6e6c939663.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -54,7 +54,7 @@ static void devfreq_max_unboost(struct work_struct *work);
 
 static struct df_boost_drv df_boost_drv_g __read_mostly = {
 	BOOST_DEV_INIT(df_boost_drv_g, DEVFREQ_CPU_LLCC_DDR_BW,
-		       3879)
+		       2597)
 };
 
 static void __devfreq_boost_kick(struct boost_dev *b)
