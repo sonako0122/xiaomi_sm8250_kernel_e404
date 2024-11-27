7156a231f0c5a8e7d5aded9681eaa285990373df
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-11-27 11:40:05 +0700

    gpu: drm_atomic: remove cpu input boost kick
    this was left unused
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,7 +34,6 @@
 #include <linux/sync_file.h>
 
 #include <linux/devfreq_boost.h>
-#include <linux/cpu_input_boost.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2609,10 +2608,9 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	/* Boost CPU and DDR when committing a new frame */
+	/* Boost DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-		cpu_input_boost_kick();
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
