43f0ae1ac707198a8bd69c23db9d737661a048e8
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-02-28 01:45:13 +0700

    fs/ntfs: completely remove debugging
    whoever write ntfs tries to capture every moment.
    i appreciate the dedication, but we don't use ntfs a lot in android and we don't need those massive debugging on every corner.
    
    also just use a placeholder instead of removing every single debug calls since there's thousand of it and im too lazy removing one per one.
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- fs/ntfs/debug.h
+++ fs/ntfs/debug.h
@@ -26,28 +26,6 @@
 
 #include "runlist.h"
 
-#ifdef DEBUG
-
-extern int debug_msgs;
-
-extern __printf(4, 5)
-void __ntfs_debug(const char *file, int line, const char *function,
-		  const char *format, ...);
-/**
- * ntfs_debug - write a debug level message to syslog
- * @f:		a printf format string containing the message
- * @...:	the variables to substitute into @f
- *
- * ntfs_debug() writes a DEBUG level message to the syslog but only if the
- * driver was compiled with -DDEBUG. Otherwise, the call turns into a NOP.
- */
-#define ntfs_debug(f, a...)						\
-	__ntfs_debug(__FILE__, __LINE__, __func__, f, ##a)
-
-extern void ntfs_debug_dump_runlist(const runlist_element *rl);
-
-#else	/* !DEBUG */
-
 #define ntfs_debug(fmt, ...)						\
 do {									\
 	if (0)								\
@@ -56,16 +34,12 @@ do {									\
 
 #define ntfs_debug_dump_runlist(rl)	do {} while (0)
 
-#endif	/* !DEBUG */
+static inline void ntfs_warning(const struct super_block *sb, const char *fmt, ...)
+{
+}
 
-extern  __printf(3, 4)
-void __ntfs_warning(const char *function, const struct super_block *sb,
-		    const char *fmt, ...);
-#define ntfs_warning(sb, f, a...)	__ntfs_warning(__func__, sb, f, ##a)
-
-extern  __printf(3, 4)
-void __ntfs_error(const char *function, const struct super_block *sb,
-		  const char *fmt, ...);
-#define ntfs_error(sb, f, a...)		__ntfs_error(__func__, sb, f, ##a)
+static inline void ntfs_error(const struct super_block *sb, const char *fmt, ...)
+{
+}
 
 #endif /* _LINUX_NTFS_DEBUG_H */
