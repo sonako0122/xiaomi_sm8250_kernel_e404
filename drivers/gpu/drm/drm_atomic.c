c9cce4714dc35380fa63be69e8861eb5322ba6ab
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-04-03 09:38:45 +0700

    treewide: enable cpu input boost as fallback
    enable back cpu input boost functionality as a fallback if scheduler fail to ramp up cpu frequency,
    this also fix microstutters in certain shit apps like telegram especially when scrolling
    
    turn this back on with a very minimum amount of boost boost and inline the duration with devfreq boost.
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -33,6 +33,7 @@
 #include <drm/drm_writeback.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2609,6 +2610,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 
 	/* Boost CPU and DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 	}
 
