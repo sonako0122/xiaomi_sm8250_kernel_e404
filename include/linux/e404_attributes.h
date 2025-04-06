77fe32261f4916b9dcc784528481333c1033a6bb
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-04-06 18:57:21 +0700

    e404_attr: switch to early_param and core_initcall
    this gives the advantage of controlling anything since e404 attr is initialized in very first place.
    tested, and values from dtbo and even cpufreq table from dtb can be overriden.
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- include/linux/e404_attributes.h
+++ include/linux/e404_attributes.h
@@ -4,10 +4,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/string.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
-#include <linux/cpufreq.h>
 
 struct e404_attributes {
     int e404_kernelsu;
@@ -23,25 +21,8 @@ struct e404_attributes {
 
 extern struct e404_attributes e404_data;
 
-static inline int extract_e404_cmdline(const char *cmdline, const char *key, u32 *value) {
-    char *ptr = strstr(cmdline, key);
-    char temp[16];
-    int ret;
-
-    if (!ptr)
-        return -1;
-
-    ptr += strlen(key);
-    sscanf(ptr, "%15s", temp);
-    ret = kstrtouint(temp, 10, value);
-    
-    if (ret) {
-        pr_alert("E404: Failed to parse %s from cmdline! Error %d\n", key, ret);
-        return ret;
-    }
-
-    pr_alert("E404: %s parsed as %d\n", key, *value);
-    return 0;
-}
+extern int e404_early_kernelsu;
+extern int e404_early_effcpu;
+extern int e404_early_rom_type;
 
 #endif /* _E404_ATTRIBUTES_H */
